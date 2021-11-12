6ef9db7a2d9b70c4b8ec77d4c050cd4d08403228
jubin jose <wow.dr.dre@gmail.com>
2021-11-12 18:48:08 +0530

    fix "covert string to buffer" warning

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ app.use(express.json({limit: '50mb'}));
 const port = 5009
 
 import Mercury from "@postlight/mercury-parser";
-import pkg from 'node-html-parser';
-const { parse } = pkg;
+// import pkg from 'node-html-parser';
+// const { parse } = pkg;
 
 app.post('/process', function (req, res) {
     if (req.body.url && req.body.html) {
         Mercury.parse(req.body.url, {
-            html: req.body.html
+            html: Buffer.from(req.body.html, 'utf8')
         }).then(result => {
-            var root = parse(result.content);
+//             var root = parse(result.content);
             res.send({
                 data: result
             });
