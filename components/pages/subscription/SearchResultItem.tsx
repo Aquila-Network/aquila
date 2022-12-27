28663440498cc81fd24bf866a8b96d128fdc3993
Maneksh M S <manekshms@gmail.com>
2022-12-27 11:54:29 +0000

    Updated links with nofollow

--- components/pages/subscription/SearchResultItem.tsx
+++ components/pages/subscription/SearchResultItem.tsx
@@ -14,10 +14,10 @@ const SearchResultItem: FC<SearchResultItemProps> = (props) => {
 	const { title, url, summary, createdAt } = props;
 	return (
 		<div className={classes["search-result-item"]}>
-			<h3 className={classes['search-result-item__title']}>{title}</h3>
+			<h3 className={classes['search-result-item__title']}><a className={classes["search-result-item__title-link"]} rel="nofollow" href={url} >{title}</a></h3>
 			<p className={classes['search-result-item__meta-info']}>Updated {moment(createdAt).fromNow()}</p>
 			<p className={classes['search-result-item__site-desc']}>{summary.length > 255 ? summary.substring(0, 255)+'...' : summary}</p>
-			<a className={classes['search-result-item__site-link']} href={url}>{url}</a>
+			<a rel="nofollow" className={classes['search-result-item__site-link']} href={url}>{url}</a>
 		</div>
 	);
 }
